0c4d706e0d6318215e8613b2bb95fdc05cfdb0dd
Horea Oros <horea.oros@gmail.com>
2015-12-10 11:59:52 +0200

    add 3n+1

---
+++ 3nPlus1/3nPlus1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3nPlus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int N;
+
+            N = Util.ExtractInt();
+
+            int lungimeSecventa;
+            lungimeSecventa = TreiNPlus1(N);
+        }
+
+        private static int TreiNPlus1(int n)
+        {
+            int contor = 1;
+
+            Console.Write("{0} ", n);
+            if (n > 0)
+            {
+                do
+                {
+                    if (n % 2 == 0)
+                    {
+                        n = n / 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                    Console.Write("{0} ", n);
+                    contor++;
+                } while (n != 1);
+                Console.WriteLine("Lungimea secventei este: {0}", contor);
+            }
+
+            return contor;
+        }
+    }
+}
---
+++ 3nPlus1/3nPlus1/Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3nPlus1
+{
+    class Util
+    {
+        internal static int ExtractInt()
+        {
+            string line;
+            Console.WriteLine("Introduceti un numar intreg");
+
+            line = Console.ReadLine();
+
+            int n = 0;
+            bool result = int.TryParse(line, out n);
+            if (result == false)
+            {
+                Console.WriteLine("Nu s-a introdus un numar corect");
+            }
+
+            return n;
+        }
+    }
+}
