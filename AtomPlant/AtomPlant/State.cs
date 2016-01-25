3fe8899205f5a8e9f9feb4046fce0f34e009cd29
Horea Oros <horea.oros@gmail.com>
2016-01-25 10:13:32 +0200

    Add Events and AtomPlant projects

---
+++ AtomPlant/AtomPlant/State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtomPlant
+{
+    class State
+    {
+        private string name;
+        AtomPlant ap;
+        public State(string s, AtomPlant ap)
+        {
+            name = s;
+            this.ap = ap;
+            this.ap.Explosion += new EventHandler<MEventArgs>(ap_Explosion);
+        }
+
+        void ap_Explosion(object sender, MEventArgs e)
+        {
+            Console.WriteLine("{0} received a notification of the explosion", name);
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
